1721c3b59f62ee91336e32f17f7681f7571e0ca1
ThevinduD <thevindud.work@gmail.com>
2025-05-01 13:18:44 +0530

    fix: handle errors & improve accessibility

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -8,25 +8,55 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
-})
+});
+
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+};
 
 const CreateInvoice = FormSchema.omit({id:true, date:true})
 
-export async function createInvoice(formdata:FormData){
-    const {customerId, amount, status} = CreateInvoice.parse ({
+export async function createInvoice(prevState:State, formdata:FormData){
+    const validatedFields = CreateInvoice.safeParse ({
         customerId: formdata.get('customerId'),
         amount: formdata.get('amount'),
         status: formdata.get('status')
     })
-    const amountInCents = amount * 100
+
+    if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql `INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    try{
+        await sql `INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    }catch(error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        }
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices');
@@ -43,9 +73,13 @@ export async function updateInvoice(id:string, formdata:FormData) {
     });
     const amountInCents = amount * 100
 
-    await sql `UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}`
+    try{
+        await sql `UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}`
+    }catch(error) {
+        console.log(error);
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices');
