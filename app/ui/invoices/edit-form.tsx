1cd0e5d8a7e2f5671cb9d4705792ef52627e755a
ThevinduD <thevindud.work@gmail.com>
2025-05-11 09:31:16 +0530

    chore: apply minor code improvements

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -34,6 +34,7 @@ export default function EditInvoiceForm({ invoice }: { invoice: InvoiceForm }) {
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
               aria-describedby="customer-error"
+              required
             >
               <option key={invoice.customer_id} value={invoice.customer_id}>
                 {invoice.name}
@@ -67,6 +68,7 @@ export default function EditInvoiceForm({ invoice }: { invoice: InvoiceForm }) {
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -96,6 +98,7 @@ export default function EditInvoiceForm({ invoice }: { invoice: InvoiceForm }) {
                   value="pending"
                   defaultChecked={invoice.status === "pending"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
                 />
                 <label
                   htmlFor="pending"
