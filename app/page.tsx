ca4e78bc207e9354ddc66c2fba638c6d80806610
ThevinduD <thevindud.work@gmail.com>
2025-07-25 21:10:37 +0530

    fix: Minor UI improvements

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,17 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-gray-950 p-4 md:h-52">
         <Logo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 lg:flex-row">
+      <div className="mt-4 flex grow flex-col gap-4 lg:flex-row animate-in duration-1000">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 lg:w-2/5 lg:px-20">
           <p
-            className={`${lusitana.className} animate-in duration-1000 text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             This is an <strong>Invoice Management System Dashboard</strong> ,
             built using Munna App Router
           </p>
           <Link
             href="/login"
-            className="animate-in duration-1000 flex items-center gap-5 self-start rounded-lg bg-gray-950 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-gray-800 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-gray-950 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-gray-800 md:text-base"
           >
             <span>Sign in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
